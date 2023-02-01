42ba7157b4775a18835630582971039fa6609e89
Matthew Smith <matthewsmith@Matthews-MacBook-Pro.local>
2023-02-01 10:21:38 -0500

    Did createActivity

--- db/activities.js
+++ db/activities.js
@@ -3,8 +3,21 @@ const client = require('./client');
 // database functions
 async function createActivity({ name, description }) {
   // return the new activity
+  try {
+    const { rows: [activity] } = await client.query(`
+    INSERT INTO activities(name, description) 
+    VALUES($1, $2) 
+    ON CONFLICT (name) DO NOTHING 
+    RETURNING *;
+  `, [name, description]);
+      
+  return activity;
+} catch (error) {
+  console.error('error creating activity')
+  throw error;
 }
-
+ }
+ 
 async function getAllActivities() {
   // select and return an array of all activities
 }
--- tests/api/users.spec.js
+++ tests/api/users.spec.js
@@ -8,7 +8,7 @@ const request = require("supertest");
 const faker = require("faker");
 const client = require("../../db/client");
 const app = require("../../app");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const {
   createFakeUserWithToken,
