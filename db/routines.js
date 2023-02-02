28a0b795d7cc5a7d63848a08b4cb3871c2db141b
asorgatz <113410935+asorgatz@users.noreply.github.com>
2023-02-02 11:21:31 -0600

    fixed seeding, some joining

--- db/routines.js
+++ db/routines.js
@@ -44,9 +44,33 @@ async function getRoutinesWithoutActivities() {
 async function getAllRoutines() {
   try {
     const {rows} = await client.query(`
-      SELECT * 
-      FROM routines 
+    SELECT routines.*, routine_activities.duration, routine_activities.count, activities.id AS "activityId", activities.name  AS "activityName", description, username AS "creatorName"
+    FROM routines
+    JOIN routine_activities ON routines.id = routine_activities."routineId"
+    JOIN activities ON activities.id = routine_activities."activityId"
+    JOIN users ON users.id = routines."creatorId"
       `)
+    console.log(rows)
+    const allRoutines = {}
+    rows.forEach( row => {
+      allRoutines[row.id] = {
+        id: row.id,
+        creatorId: row.creatorId,
+        isPublic: row.isPublic,
+        name: row.name,
+        goal: row.goal,
+        activities: {
+
+          duration: row.duration,
+
+        }
+
+
+
+      }
+    })
+
+
     return rows
   } catch (error) {
     throw error
@@ -71,7 +95,23 @@ async function getPublicRoutinesByUser({ username }) {}
 
 async function getPublicRoutinesByActivity({ id }) {}
 
-async function updateRoutine({ id, ...fields }) {}
+async function updateRoutine({ id, ...fields }) {
+  const setString = Object.keys(fields).map(
+    (key, index) => `"${ key }"=$${ index + 1 }`
+  ).join(', ');
+    console.log(setString)
+  try {
+    const {rows: [activity]} = await client.query(`
+    UPDATE routines
+    SET ${ setString }
+    WHERE id=${ id }
+    RETURNING *
+    `, Object.values(fields))
+    return activity
+  } catch (error) {
+    throw error
+  }
+}
 
 async function destroyRoutine(id) {}
 
